e246dd7a5c6797cc4687d2a18da68b6ab2e89f6c
hulichao <jsu_hlc@163.com>
2020-09-12 16:20:41 +0800

    hadoop 2.9.2

--- docker-hadoop/conf/hadoop-env.sh
+++ docker-hadoop/conf/hadoop-env.sh
@@ -21,7 +21,7 @@
 # remote nodes.
 
 # The java implementation to use.
-export JAVA_HOME=/usr/local/jdk1.8
+export JAVA_HOME=/opt/hoult/servers/jdk1.8.0_151
 
 # The jsvc implementation to use. Jsvc is required to run secure datanodes.
 #export JSVC_HOME=${JSVC_HOME}
