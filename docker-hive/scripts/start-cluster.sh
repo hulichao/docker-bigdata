a7df066de8f3c150d059d7d3105d34a74ffa61ee
hulichao <hulichao@duxiaoman.com>
2020-10-08 14:27:50 +0800

    8288

--- docker-hive/scripts/start-cluster.sh
+++ docker-hive/scripts/start-cluster.sh
@@ -1,5 +1,5 @@
 ## 启动hdfs和yarn
-#ssh root@linux121 "cd /opt/hoult/servers/hadoop-2.9.2/ && bin/hadoop namenode -format "
+ssh root@linux121 "cd /opt/hoult/servers/hadoop-2.9.2/ && bin/hadoop namenode -format "
 ssh root@linux121 "cd /opt/hoult/servers/hadoop-2.9.2/ && sbin/start-dfs.sh && sbin/mr-jobhistory-daemon.sh start historyserver"
 ssh root@linux123 "cd /opt/hoult/servers/hadoop-2.9.2/ && sbin/start-yarn.sh"
 ssh root@linux121  "nohup hive --service metastore & " ## 远程模式启动hive的元数据服务
