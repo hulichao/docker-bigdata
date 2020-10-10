209da96784d1be8c8cd14a770da09905a1da320d
hulichao <hulichao@duxiaoman.com>
2020-10-10 18:11:02 +0800

    hive-site.xml

--- docker-hive/scripts/start-cluster.sh
+++ docker-hive/scripts/start-cluster.sh
@@ -4,6 +4,6 @@ ssh root@linux121 "cd /opt/hoult/servers/hadoop-2.9.2/ && sbin/start-dfs.sh && s
 ssh root@linux123 "cd /opt/hoult/servers/hadoop-2.9.2/ && sbin/start-yarn.sh"
 ## 初始化hive元数据 && 远程模式启动hive的元数据服务
 ssh root@linux121 "/opt/hoult/servers/hive-2.3.7/bin/schematool -dbType mysql -initSchema" && \
-ssh root@linux121  "nohup /opt/hoult/servers/hive-2.3.7/bin/hive --service metastore &" && \
+ssh root@linux121  "nohup /opt/hoult/servers/hive-2.3.7/bin/hive --service metastore &"
 ssh root@linux123  "nohup /opt/hoult/servers/hive-2.3.7/bin/hive --service metastore &"
 
