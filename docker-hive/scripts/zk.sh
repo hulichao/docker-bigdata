afb1d38ec7aa1e90d53397f6969adeb0caa69152
hulichao <hulichao@duxiaoman.com>
2020-10-24 15:37:55 +0800

    zk.sh

---
+++ docker-hive/scripts/zk.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+echo "start zookeeper server..."
+if(($#==0));then
+echo "no params";
+exit;
+fi
+hosts="linux121 linux122 linux123"
+for host in $hosts
+do
+ssh $host "source /etc/profile; /opt/hoult/servers/zookeeper-
+3.4.14/bin/zkServer.sh $1"
+done
