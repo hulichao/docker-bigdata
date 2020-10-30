a5fee6cfcb204f11dd4a9fa62240cb1b0916a587
hulichao <jsu_hlc@163.com>
2020-10-30 23:05:06 +0800

    update

--- docker-hive/scripts/stop-cluster.sh
+++ docker-hive/scripts/stop-cluster.sh
@@ -2,5 +2,7 @@
 ssh root@linux121 "cd /opt/hoult/servers/hadoop-2.9.2/ && sbin/stop-dfs.sh && sbin/mr-jobhistory-daemon.sh stop historyserver"
 ssh root@linux123 "cd /opt/hoult/servers/hadoop-2.9.2/ && sbin/stop-yarn.sh"
 ssh root@linux121 "/opt/hoult/servers/hbase-1.3.1/bin/stop-hbase.sh"
+ssh root@linux121 "/opt/hoult/servers/scripts/zk.sh stop"
+
 #ssh root@linux121 "/opt/hoult/servers/hive-2.3.7/bin/schematool -dbType mysql -initSchema"
 
