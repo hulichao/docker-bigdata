fd8bc4982a7cc9754c96f62f3fc115cb4c13d402
hulichao <jsu_hlc@163.com>
2020-11-14 15:02:09 +0800

    dev-commit.sh

---
+++ dev-commit.sh
@@ -0,0 +1,10 @@
+## endi
+
+git add -u .
+git commit -m "update"
+git push origin dev
+
+docker commit -a "hulichao" -m "update a little" linux121 hoult/linux121
+docker commit -a "hulichao" -m "update a little" linux122 hoult/linux122
+docker commit -a "hulichao" -m "update a little" linux123 hoult/linux123
+
--- docker-hive/scripts/stop-cluster.sh
+++ docker-hive/scripts/stop-cluster.sh
@@ -3,6 +3,7 @@ ssh root@linux121 "cd /opt/hoult/servers/hadoop-2.9.2/ && sbin/stop-dfs.sh && sb
 ssh root@linux123 "cd /opt/hoult/servers/hadoop-2.9.2/ && sbin/stop-yarn.sh"
 ssh root@linux121 "/opt/hoult/servers/hbase-1.3.1/bin/stop-hbase.sh"
 ssh root@linux121 "/opt/hoult/servers/scripts/zk.sh stop"
+ssh root@linux121 "redis-cli shutdown"
 
 #ssh root@linux121 "/opt/hoult/servers/hive-2.3.7/bin/schematool -dbType mysql -initSchema"
 
