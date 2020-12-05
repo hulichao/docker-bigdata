9b7833cf6f8683ada2e339697293b81d508f7902
hulichao <jsu_hlc@163.com>
2020-12-05 23:58:16 +0800

    修改hive 默认引擎为tez

--- docker-hive/scripts/start-cluster.sh
+++ docker-hive/scripts/start-cluster.sh
@@ -5,9 +5,9 @@ ssh root@linux123 "cd /opt/hoult/servers/hadoop-2.9.2/ && sbin/start-yarn.sh"
 
 ## 初始化hive元数据 && 远程模式启动hive的元数据服务
 # ssh root@linux121 "/opt/hoult/servers/hive-2.3.7/bin/schematool -dbType mysql -initSchema" && \
-ssh root@linux121 "/opt/hoult/servers/scripts/zk.sh start"
-ssh root@linux121 "/opt/hoult/servers/hbase-1.3.1/bin/start-hbase.sh"
-ssh root@linux121 "redis-server /usr/redis/bin/redis.conf"
+#ssh root@linux121 "/opt/hoult/servers/scripts/zk.sh start"
+#ssh root@linux121 "/opt/hoult/servers/hbase-1.3.1/bin/start-hbase.sh"
+#ssh root@linux121 "redis-server /usr/redis/bin/redis.conf"
 #ssh root@linux121  "nohup /opt/hoult/servers/hive-2.3.7/bin/hive --service metastore &" && \
 #ssh root@linux123  "nohup /opt/hoult/servers/hive-2.3.7/bin/hive --service metastore &" && \
 #ssh root@linux123  "nohup /opt/hoult/servers/hive-2.3.7/bin/hiveserver2 &"
