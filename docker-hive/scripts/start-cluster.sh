1e7dd6f57baf303dd7a6e46ebcbe134aff70f1fc
hulichao <jsu_hlc@163.com>
2020-12-13 18:56:08 +0800

    start

--- docker-hive/scripts/start-cluster.sh
+++ docker-hive/scripts/start-cluster.sh
@@ -8,11 +8,12 @@ ssh root@linux123 "cd /opt/hoult/servers/hadoop-2.9.2/ && sbin/start-yarn.sh"
 #ssh root@linux121 "/opt/hoult/servers/scripts/zk.sh start"
 #ssh root@linux121 "/opt/hoult/servers/hbase-1.3.1/bin/start-hbase.sh"
 #ssh root@linux121 "redis-server /usr/redis/bin/redis.conf"
-#ssh root@linux121  "nohup /opt/hoult/servers/hive-2.3.7/bin/hive --service metastore >> /dev/null &" && \
-#ssh root@linux123  "nohup /opt/hoult/servers/hive-2.3.7/bin/hive --service metastore >> /dev/null &" && \
-#ssh root@linux123  "nohup /opt/hoult/servers/hive-2.3.7/bin/hiveserver2 >> /dev/null &"
+ssh root@linux121  "nohup /opt/hoult/servers/hive-2.3.7/bin/hive --service metastore >> /dev/null &" && \
+ssh root@linux123  "nohup /opt/hoult/servers/hive-2.3.7/bin/hive --service metastore >> /dev/null &" && \
+ssh root@linux121  "nohup /opt/hoult/servers/hive-2.3.7/bin/hiveserver2 >> /dev/null &"
+ssh root@linux123  "nohup /opt/hoult/servers/hive-2.3.7/bin/hiveserver2 >> /dev/null &"
 
 # 启动hue
-#ssh root@linux122  "nohup /opt/hoult/servers/hue/build/env/bin/supervisor >> /dev/null &"
+ssh root@linux122  "nohup /opt/hoult/servers/hue/build/env/bin/supervisor >> /dev/null &"
 
 
