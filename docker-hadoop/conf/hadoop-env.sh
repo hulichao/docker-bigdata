a810c849cccf02a4e13637f165197846f6b921ad
hulichao <jsu_hlc@163.com>
2020-12-14 19:03:54 +0800

    update

--- docker-hadoop/conf/hadoop-env.sh
+++ docker-hadoop/conf/hadoop-env.sh
@@ -82,14 +82,3 @@ export HADOOP_HOME=/opt/hoult/servers/hadoop-2.9.2
 # Hive Configuration Directory can be controlled by:
 export HIVE_CONF_DIR=/opt/hoult/servers/hive-2.3.7/conf
 
-# Folder containing extra libraries required for hive compilation/execution can be controlled by:
-export TEZ_HOME=/opt/hoult/servers/tez-0.9.2
-export TEZ_JARS=""
-for jar in `ls $TEZ_HOME |grep jar`; do
-    export TEZ_JARS=$TEZ_JARS:$TEZ_HOME/$jar
-done
-for jar in `ls $TEZ_HOME/lib`; do
-    export TEZ_JARS=$TEZ_JARS:$TEZ_HOME/lib/$jar
-done
-
-export HIVE_AUX_JARS_PATH=$TEZ_JARS
