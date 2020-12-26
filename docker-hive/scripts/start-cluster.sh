c12a37e9e4dc4b37291303d7572b536cfa211de5
hulichao <hulichao@duxiaoman.com>
2020-12-26 11:46:08 +0800

    hiveserver2

--- docker-hive/scripts/start-cluster.sh
+++ docker-hive/scripts/start-cluster.sh
@@ -4,13 +4,15 @@ ssh root@linux123 "cd /opt/hoult/servers/hadoop-2.9.2/ && sbin/start-yarn.sh"
 # ssh root@linux121 "cd /opt/hoult/servers/hadoop-2.9.2/ && bin/hadoop namenode -format "
 
 ## 初始化hive元数据 && 远程模式启动hive的元数据服务
-# ssh root@linux121 "/opt/hoult/servers/hive-2.3.7/bin/schematool -dbType mysql -initSchema" && \
+# ssh root@linux123 "/opt/hoult/servers/hive-2.3.7/bin/schematool -dbType mysql -initSchema" && \
 #ssh root@linux121 "/opt/hoult/servers/scripts/zk.sh start"
 #ssh root@linux121 "/opt/hoult/servers/hbase-1.3.1/bin/start-hbase.sh"
 #ssh root@linux121 "redis-server /usr/redis/bin/redis.conf"
 ssh root@linux123  "nohup /opt/hoult/servers/hive-2.3.7/bin/hive --service metastore >> /dev/null &" && \
 ssh root@linux123  "nohup /opt/hoult/servers/hive-2.3.7/bin/hiveserver2 >> /dev/null &"
 
+## hiveserver2 带日志的启动
+#./hive --service hiveserver2 --hiveconf hive.server2.thrift.port=10000 --hiveconf hive.root.logger=DEBUG,console
 # 启动hue
 ssh root@linux122  "nohup /opt/hoult/servers/hue/build/env/bin/supervisor >> /dev/null &"
 
