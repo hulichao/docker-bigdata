a6306f8f25ed5a83752ddc7534f0865316fa5d14
hulichao <jsu_hlc@163.com>
2021-02-01 22:39:49 +0800

    kafka 群起脚本

--- docker-hive/scripts/kafka.sh
+++ docker-hive/scripts/kafka.sh
@@ -1,1 +1,3 @@
-/opt/hoult/servers/kafka-2.12/bin/kafka-server-$1.sh -daemon /opt/hoult/servers/kafka-2.12/config/server.properties
+ssh root@linux121  "/opt/hoult/servers/kafka-2.12/bin/kafka-server-$1.sh -daemon /opt/hoult/servers/kafka-2.12/config/server.properties"
+ssh root@linux122  "/opt/hoult/servers/kafka-2.12/bin/kafka-server-$1.sh -daemon /opt/hoult/servers/kafka-2.12/config/server.properties"
+ssh root@linux123  "/opt/hoult/servers/kafka-2.12/bin/kafka-server-$1.sh -daemon /opt/hoult/servers/kafka-2.12/config/server.properties"
