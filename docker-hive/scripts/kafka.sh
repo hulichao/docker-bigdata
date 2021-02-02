92dff4d760373cff485017b90475ef8c5b14e60b
hulichao <jsu_hlc@163.com>
2021-02-03 00:08:26 +0800

    update

--- docker-hive/scripts/kafka.sh
+++ docker-hive/scripts/kafka.sh
@@ -1,3 +1,3 @@
-ssh root@linux121  "/opt/hoult/servers/kafka-2.12/bin/kafka-server-$1.sh -daemon /opt/hoult/servers/kafka-2.12/config/server.properties"
-ssh root@linux122  "/opt/hoult/servers/kafka-2.12/bin/kafka-server-$1.sh -daemon /opt/hoult/servers/kafka-2.12/config/server.properties"
-ssh root@linux123  "/opt/hoult/servers/kafka-2.12/bin/kafka-server-$1.sh -daemon /opt/hoult/servers/kafka-2.12/config/server.properties"
+ssh root@linux121  "source /etc/profile && /opt/hoult/servers/kafka-2.12/bin/kafka-server-$1.sh -daemon /opt/hoult/servers/kafka-2.12/config/server.properties"
+ssh root@linux122  "source /etc/profile && /opt/hoult/servers/kafka-2.12/bin/kafka-server-$1.sh -daemon /opt/hoult/servers/kafka-2.12/config/server.properties"
+ssh root@linux123  " source /etc/profile && /opt/hoult/servers/kafka-2.12/bin/kafka-server-$1.sh -daemon /opt/hoult/servers/kafka-2.12/config/server.properties"
