81f96bba7ab83dcbd93fa814b120c36fbc7d5dd6
hulichao <jsu_hlc@163.com>
2021-02-16 18:24:29 +0800

    cdh搭建初版

--- run.sh
+++ run.sh
@@ -16,5 +16,12 @@ docker run -it -d --rm --name=linux121 -h linux121 --net dockerbigdata_zoo --pri
 docker run -it -d --rm --name=linux122 -h linux122 --net dockerbigdata_zoo --privileged hoult/linux122
 docker run -it -d --rm --name=linux123 -h linux123 --net dockerbigdata_zoo --privileged hoult/linux123
 
+### cdh的启动
+docker run -it -d --rm --name=hadoop1 -h hadoop1 --net dockerbigdata_zoo --ip 172.19.0.21 --privileged hoult/centos7:jdk8-ssh
+docker run -it -d --rm --name=hadoop2 -h hadoop2 --net dockerbigdata_zoo --ip 172.19.0.22 --privileged hoult/centos7:jdk8-ssh
+docker run -it -d --rm --name=hadoop3 -h hadoop3 --net dockerbigdata_zoo --ip 172.19.0.23 --privileged hoult/centos7:jdk8-ssh
+docker run -it -d --rm --name=hadoop4 -h hadoop4 --net dockerbigdata_zoo --ip 172.19.0.24 --privileged hoult/centos7:jdk8-ssh
+docker run -it -d --rm --name=hadoop5 -h hadoop5 --net dockerbigdata_zoo --ip 172.19.0.25 --privileged hoult/centos7:jdk8-ssh
+
 ## hue start
 build/env/bin/supervisor
