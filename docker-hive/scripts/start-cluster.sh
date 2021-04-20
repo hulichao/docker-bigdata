3acf5edf1819a32892cbd42f8bf6351e5ccadff7
hulichao <jsu_hlc@163.com>
2021-04-21 01:32:20 +0800

    start cluster update

--- docker-hive/scripts/start-cluster.sh
+++ docker-hive/scripts/start-cluster.sh
@@ -8,8 +8,8 @@ ssh root@linux121 "cd /opt/hoult/servers/hadoop-2.9.2/ && sbin/start-dfs.sh && s
 #ssh root@linux121 "/opt/hoult/servers/scripts/zk.sh start"
 #ssh root@linux121 "/opt/hoult/servers/hbase-1.3.1/bin/start-hbase.sh"
 #ssh root@linux121 "redis-server /usr/redis/bin/redis.conf"
-ssh root@linux123  "nohup /opt/hoult/servers/hive-2.3.7/bin/hive --service metastore >> /dev/null &" && \
-ssh root@linux123  "nohup /opt/hoult/servers/hive-2.3.7/bin/hiveserver2 >> /dev/null &"
+#ssh root@linux123  "nohup /opt/hoult/servers/hive-2.3.7/bin/hive --service metastore >> /dev/null &" && \
+#ssh root@linux123  "nohup /opt/hoult/servers/hive-2.3.7/bin/hiveserver2 >> /dev/null &"
 
 ## hiveserver2 带日志的启动
 #./hive --service hiveserver2 --hiveconf hive.server2.thrift.port=10000 --hiveconf hive.root.logger=DEBUG,console
@@ -17,6 +17,6 @@ ssh root@linux123  "nohup /opt/hoult/servers/hive-2.3.7/bin/hiveserver2 >> /dev/
 #ssh root@linux122  "nohup /opt/hoult/servers/hue/build/env/bin/supervisor >> /dev/null &"
 
 ## 启动Spark相关
-sh /opt/hoult/servers/scripts/start-spark.sh
+#sh /opt/hoult/servers/scripts/start-spark.sh
 
 
