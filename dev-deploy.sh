ffcc615afeb7602de0292402438e63ee13477a75
hulichao <jsu_hlc@163.com>
2021-05-28 01:49:06 +0800

    cdh 不免费了，没有搭建起来，改为原来三台机器

--- dev-deploy.sh
+++ dev-deploy.sh
@@ -5,21 +5,21 @@ git add -u .
 git commit -m "update"
 git push origin dev
 
-#docker commit -a "hulichao" -m "update a little" linux121 hoult/linux121
-#docker push hoult/linux121
-#docker commit -a "hulichao" -m "update a little" linux122 hoult/linux122
-#docker push hoult/linux122
-#docker commit -a "hulichao" -m "update a little" linux123 hoult/linux123
-#docker push hoult/linux123
+docker commit -a "hulichao" -m "update a little" linux121 hoult/linux121
+docker push hoult/linux121
+docker commit -a "hulichao" -m "update a little" linux122 hoult/linux122
+docker push hoult/linux122
+docker commit -a "hulichao" -m "update a little" linux123 hoult/linux123
+docker push hoult/linux123
 
-docker commit -a "hulichao" -m "update a little" hadoop1 hoult/hadoop1
-docker push hoult/hadoop1
-docker commit -a "hulichao" -m "update a little" hadoop2 hoult/hadoop2
-docker push hoult/hadoop2
-docker commit -a "hulichao" -m "update a little" hadoop3 hoult/hadoop3
-docker push hoult/hadoop3
-docker commit -a "hulichao" -m "update a little" hadoop4 hoult/hadoop4
-docker push hoult/hadoop4
-docker commit -a "hulichao" -m "update a little" hadoop5 hoult/hadoop5
-docker push hoult/hadoop5
+#docker commit -a "hulichao" -m "update a little" hadoop1 hoult/hadoop1
+#docker push hoult/hadoop1
+#docker commit -a "hulichao" -m "update a little" hadoop2 hoult/hadoop2
+#docker push hoult/hadoop2
+#docker commit -a "hulichao" -m "update a little" hadoop3 hoult/hadoop3
+#docker push hoult/hadoop3
+#docker commit -a "hulichao" -m "update a little" hadoop4 hoult/hadoop4
+#docker push hoult/hadoop4
+#docker commit -a "hulichao" -m "update a little" hadoop5 hoult/hadoop5
+#docker push hoult/hadoop5
 
